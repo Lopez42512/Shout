f07fcf2ae396d562bd2cb96ebb5f3fe5eb2ca054
Jorge <3d.jorgesierra@gmail.com>
2019-03-21 22:04:54 -0400

    cleaned some code

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,4 @@
-https://api.yelp.com/v3/businesses/search
+//https://api.yelp.com/v3/businesses/search
 // GET https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
 //https://cors-anywhere.herokuapp.com/
 
@@ -20,7 +20,7 @@ function getGeoLocation() {
 
             //display lattitude and longitude in dom
             var pLocation = $("<p>");
-            pLocation.text("The Lattitude: " + userLocation[0] + " and the longitude: " + userLocation[1]);
+            pLocation.text("The Lattitude-array: " + userLocation[0] + " and the longitude-array: " + userLocation[1]);
 
             $("body").append(pLocation);
         });
@@ -28,13 +28,13 @@ function getGeoLocation() {
         console.log("no access to geto")
     }
 
-    console.log(userLocation[0] + "LONG: " + userLocation[1]);
+    console.log("console lat - " + userLocation[0] + " console long - " + userLocation[1]);
 
     
 // this is the call for YELP QUERY
-var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=39.951061&longitude=-75.165619&radius=8046.72";
+// var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=39.951061&longitude=-75.165619&radius=8046.72";
 //testing to see if I can get variables included
-// var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=." + userLocation[0] + "&longitude=." + userLocation[1] + "&radius=8046.72";
+var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=" + userLocation[0] + "&longitude=" + userLocation[1] + "&radius=8046.72";
 var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0AdLgu-HHoa2cM4m1WaAefYoboIPdVHv0mCjivrwQrdU11FCFl2hd8-iaaTKOTXHYx";
 
 
@@ -50,7 +50,6 @@ var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0Ad
         }
 
     );
-
 }
 
 $(document).on("click", "#getGeoLocation", getGeoLocation);
