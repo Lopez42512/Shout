af5e032ebaed3ff4409d01aa6f63e1d34c530de9
Jorge <3d.jorgesierra@gmail.com>
2019-03-21 21:09:07 -0400

    working yelp request; map is still not working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,82 +1,66 @@
-//AJAX Call for YELP API
+https://api.yelp.com/v3/businesses/search
 // GET https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
 //https://cors-anywhere.herokuapp.com/
-var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972&radius=8046.72";
-var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0AdLgu-HHoa2cM4m1WaAefYoboIPdVHv0mCjivrwQrdU11FCFl2hd8-iaaTKOTXHYx";
 
-var lat;
-var long;
+//Global variables
+var userLocation = [];
 var gMap;
-// JAVASCRIPT MAP API AND QUERY 
 
 // var googleMapQuery = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA4Xg_9AqzPbXG547Ie66bFn-kqaYduOe0" >
 
-    // Just using HTML API for geo location and test it with other APIs
-    function getGeoLocation() {
+// Just using HTML API for geo location and test it with other APIs
+function getGeoLocation() {
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position);
-                lat = position.coords.latitude;
-                long = position.coords.longitude;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            
+            //Push Latitude and Longitude
+            userLocation.push(position.coords.latitude);
+            userLocation.push(position.coords.longitude);
 
-                var pLocation = $("<p>");
-                pLocation.text("The Lattitude: " + lat + " and the longitude: " + long);
+            //display lattitude and longitude in dom
+            var pLocation = $("<p>");
+            pLocation.text("The Lattitude: " + userLocation[0] + " and the longitude: " + userLocation[1]);
 
-                $("body").append(pLocation);
-            });
-        } else {
-            console.log("no access to geto")
-        }
+            $("body").append(pLocation);
+        });
+    } else {
+        console.log("no access to geto")
     }
+
+    console.log(userLocation[0] + "LONG: " + userLocation[1]);
+
+    
+// this is the call for YELP QUERY
+var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=39.951061&longitude=-75.165619&radius=8046.72";
+//testing to see if I can get variables included
+// var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=." + userLocation[0] + "&longitude=." + userLocation[1] + "&radius=8046.72";
+var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0AdLgu-HHoa2cM4m1WaAefYoboIPdVHv0mCjivrwQrdU11FCFl2hd8-iaaTKOTXHYx";
+
+
+    console.log(yelpQuery);
+    $.ajax({
+        url: yelpQuery,
+        headers: {
+            'Authorization': "Bearer " + yelpAPI,
+        },
+        method: "GET"
+    }).then((yelpResponse) => {
+            console.log(yelpResponse);
+        }
+
+    );
+
+}
 
 $(document).on("click", "#getGeoLocation", getGeoLocation);
 
-$.ajax({
-    url: yelpQuery,
-    headers: {
-        'Authorization': "Bearer " + yelpAPI,
-    },
-    method: "GET"
-}).then((yelpResponse) => {
-        console.log(yelpResponse);
-    }
+// getGeoLocation();
 
-);
 
-// function initMap() {
-
-//     // the location of the person
-//     var userLocation = {
-//         lar: lat,
-//         lng: long
-//     };
-//     //The map, Centered to the person
-//     gMap = new google.maps.Map(document.getElementById("map"), {
-//         center: userLocation,
-//         zoom: 20
-//     });
-//     // position the marker on the location of the person
-//     var marker = new google.maps.Marker({
-//         position: userLocation,
-//         map: gMap
-//     });
-
-// }
-
-// // Javascript map API
-// $.ajax({
-//     url: googleMapQuery,
-//     method: "GET"
-// }, then((googleResponse) => {
-
-//     initMap();
-//     console.log(googleResponse);
-// }));
 
 //firebase Google Maps API
 //https://www.google.com/maps/embed/v1/MODE?key=AIzaSyCHREkl-BVxFLXW5Hp_reWCSnIDK-Oq2Wk&parameters
-
 
 
 // Execute functions
