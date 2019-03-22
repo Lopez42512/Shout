a63b8ef9a76cda97c45b4f31ae68e78a3c89a8d9
Jorge <3d.jorgesierra@gmail.com>
2019-03-22 13:24:22 -0400

    working map and Yelp Query Static

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,20 @@
-//https://api.yelp.com/v3/businesses/search
-// GET https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
-//https://cors-anywhere.herokuapp.com/
-
 //Global variables
 var userLocation = [];
 var gMap;
+//--------
+
 
 // var googleMapQuery = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA4Xg_9AqzPbXG547Ie66bFn-kqaYduOe0" >
 
 // Just using HTML API for geo location and test it with other APIs
 function getGeoLocation() {
 
-    userLocation =[];
-    
+    userLocation = [];
+
     // place inside function then query for it.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
-            
+
             //Push Latitude and Longitude
             userLocation.push(position.coords.latitude);
             userLocation.push(position.coords.longitude);
@@ -33,12 +31,13 @@ function getGeoLocation() {
 
     console.log("console lat - " + userLocation[0] + " console long - " + userLocation[1]);
 
-    
-// this is the call for YELP QUERY
-// var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=39.951061&longitude=-75.165619&radius=8000";
-//testing to see if I can get variables included
-var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=" + userLocation[0] + "&longitude=" + userLocation[1] + "&radius=8000";
-var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0AdLgu-HHoa2cM4m1WaAefYoboIPdVHv0mCjivrwQrdU11FCFl2hd8-iaaTKOTXHYx";
+
+    // this is the call for YELP QUERY - WORKING
+    var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=39.951061&longitude=-75.165619&radius=8000";
+
+    //testing  to get variables -- Needs WORK!
+    // var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=" + userLocation[0] + "&longitude=" + userLocation[1] + "&radius=8000";
+    var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0AdLgu-HHoa2cM4m1WaAefYoboIPdVHv0mCjivrwQrdU11FCFl2hd8-iaaTKOTXHYx";
 
 
     console.log(yelpQuery);
@@ -56,10 +55,6 @@ var yelpAPI = "1QpSc4B1zI5GuI56PDAAvAfpfcsLg9LWuHRfVCeG4TIDDxRe3hGT-sxlU5h5DD0Ad
 }
 
 $(document).on("click", "#getGeoLocation", getGeoLocation);
-
-// getGeoLocation();
-
-
 
 //firebase Google Maps API
 //https://www.google.com/maps/embed/v1/MODE?key=AIzaSyCHREkl-BVxFLXW5Hp_reWCSnIDK-Oq2Wk&parameters
